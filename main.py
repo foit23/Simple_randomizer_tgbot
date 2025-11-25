f76f02be133f0805c34372d021eaf2c3ab733d67
foit23 <mironilusenko19@gmail.com>
2025-11-25 21:21:50 +0300

    updates
    update

---
+++ main.py
@@ -0,0 +1,22 @@
+import aiogram
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import asyncio
+from aiogram.enums import DiceEmoji
+
+bot = Bot(token = "8326658640:AAHQP51BPk9oZBdlfNNqL60_iuO16TN4sgI")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def starts(message = types.Message):
+    await message.answer("Бот который просто кидает игральную кость, для броска введите команду /Reroll \n Автор бота: @foit_23")
+
+@dp.message(Command("Reroll"))
+async def reroll(message = types.Message):
+    await message.answer_dice(emoji = DiceEmoji.DICE)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
